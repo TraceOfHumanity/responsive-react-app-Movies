668f6714f97eee5598ed1242640262d9f7c83ec5
AzAlex <azalexxx02@gmail.com>
2022-11-02 20:08:21 -0700

    button play

--- src/components/home/HomeCard.jsx
+++ src/components/home/HomeCard.jsx
@@ -7,9 +7,56 @@ const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genre
       <div className="box">
         <div className="coverImage">
           <img src={cover} alt="" />
-          <h1>{id}</h1>
-          <h1>{tags}</h1>
-          <h1>{time}</h1>
+        </div>
+        <div className="content flex">
+          <div className="details row">
+            <h1>{name}</h1>
+            <div className="rating flex">
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star"></i>
+              <i className="fa fa-star-half"></i>
+            </div>
+            <label>{rating}</label>
+            <span>GP</span>
+            <label>{time}</label>
+          </div>
+          <p>{desc}</p>
+          <div className="cost">
+            <h4>
+              <span>
+                Starring
+                {starring}
+              </span>
+            </h4>
+            <h4>
+              <span>
+                Genres
+                {genres}
+              </span>
+            </h4>
+            <h4>
+              <span>
+                Tags
+                {tags}
+              </span>
+            </h4>
+            <button className='primary-btn'>
+              <i className="fas fa-play"></i>PLAY NOW
+            </button>
+            <div className="button row">
+              <Link to={`/singlepage/${id}`}>
+                <button>
+                  <div className="img">
+                    <img src="./images/play-button.png" alt="" />
+                    <img src="./images/play.png" alt="" />
+                  </div>
+                  WATCH TRAILER
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
