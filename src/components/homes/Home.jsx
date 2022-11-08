e72ca0f316dbd129e4db076557d8f693f45e6f53
AzAlex <azalexxx02@gmail.com>
2022-11-08 08:31:06 -0800

    28:29

--- src/components/homes/Home.jsx
+++ src/components/homes/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import HomeCard from './HomeCard'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Home = ({ items }) => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <div>
       <div className="homeContainer">
-        {items.map((item) => (
-          <HomeCard key={item.id} item={item} />
-        ))}
+        <Slider {...settings}>
+          {items.map((item) => (
+            <HomeCard key={item.id} item={item} />
+          ))}
+        </Slider>
       </div>
     </div>
   )
